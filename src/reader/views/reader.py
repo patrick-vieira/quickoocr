54e09a070169960d28fce9161b9db0deeee07b27
Patrick Vieira <pdsv88@gmail.com>
2022-08-06 13:08:16 -0300

    image reading

--- src/reader/views/reader.py
+++ src/reader/views/reader.py
@@ -1,10 +1,21 @@
+import re
+
 from django.shortcuts import render
+
+from PIL import Image
+from io import BytesIO
+import base64
+import pytesseract
 
 
 def read(request):
-    result = "temp"
 
-    image64 = request.POST
+    image_data = re.sub('^data:image/.+;base64,', '', request.POST['base64'])
+    image = Image.open(BytesIO(base64.b64decode(image_data)))
+
+    result = pytesseract.image_to_string(image=image)
+
+    print(result)
 
     return render(request, "home/read_result.html", {
         'result': result
